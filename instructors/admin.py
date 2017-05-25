7a1e8668b855f58b875a5e6960c1af1c53d8a583
israodin <israodin@gmail.com>
2017-05-25 23:19:43 +0300

    django week3

--- instructors/admin.py
+++ instructors/admin.py
@@ -1,9 +1,42 @@
 from django.contrib import admin
 from instructors.models import Instructor,Position,Course
+from django.db import models
+from django.forms import widgets
 # Register your models here.
 
-admin.site.register(Instructor)
-admin.site.register(Position)
+
+class InstructorInline(admin.StackedInline):
+    model = Instructor
+    fields = ["name","surname"]
+
+class PositionAdmin(admin.ModelAdmin):
+    inlines = [InstructorInline]
+
+class InstructorAdmin(admin.ModelAdmin):
+    list_display = ["name","surname","is_active","position"]
+    list_display_links = ["name","surname"]
+
+    list_filter = ["is_active","position"]
+    search_fields = ["name","surname"]
+    list_editable = ["is_active"]
+    # fields = ["name","surname","email","position","is_active","date_of_birthday"]
+    # exclude = []
+    readonly_fields = ['is_active']
+    raw_id_fields = ['position']
+    save_as = True
+    save_on_top = True
+    fieldsets = [
+        (None,{'fields':['name','surname']}),
+        ('Other information',{'fields':['email','date_of_birthday',
+                                        'position','is_active'],'classes':['collapse']}),
+    ]
+    formfield_overrides = {
+        models.DateField:{'widget':widgets.TextInput}
+    }
+admin.site.register(Instructor,InstructorAdmin)
+admin.site.register(Position,PositionAdmin)
 admin.site.register(Course)
+
+
 
 
